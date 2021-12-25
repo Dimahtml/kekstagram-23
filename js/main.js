9f8eb4c3cc51b613e3f86cdeab07408b283ada97
Dimahtml <bomabo@mail.ru>
2021-12-25 14:59:36 +0500

    новый модуль pictures, который отрисовывает фотографии

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-import { createPictures } from './data.js';
-
-const similarPictures = createPictures();
-
-// eslint-disable-next-line no-console
-console.log(similarPictures);
+import './pictures.js';
