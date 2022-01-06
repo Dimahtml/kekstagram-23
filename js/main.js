7d15fb4d4f6d7d65985449629d17106248d1be92
Dimahtml <bomabo@mail.ru>
2022-01-06 13:12:48 +0500

    используются данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 import './pictures.js';
 import './form.js';
-// import './picture-effects.js';
+import { renderSimilarPictures } from './pictures.js';
+
+fetch('https://23.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((pictures) => {
+    renderSimilarPictures(pictures);
+  });
